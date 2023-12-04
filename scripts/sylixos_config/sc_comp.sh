6fade92c226fea5393bfdebc6055e92af45123c1
ZhaoBing <zhaobing@acoinfo.com>
2023-12-04 21:55:09 +0800

    Added sc.

---
+++ scripts/sylixos_config/sc_comp.sh
@@ -0,0 +1,27 @@
+function comp_sc()
+{
+    COMPREPLY=()
+    local cur=${COMP_WORDS[COMP_CWORD]}
+    local cmd=${COMP_WORDS[COMP_CWORD-1]}
+
+    case $cmd in
+    'sc')
+        COMPREPLY=( $(compgen -W 'base build' $cur ) )
+    ;;
+
+    'base')
+        work_space_path="${HOME}/works/sylixos-workspace"
+        bases=""
+        for item in $work_space_path/bases/*; do
+            temp=${item##*/}
+            bases=$bases" ${temp}"
+        done
+
+        COMPREPLY=( $(compgen -W '${bases}' $cur ) )
+    ;;
+
+    esac
+
+    return 0
+}
+complete -F comp_sc sc
